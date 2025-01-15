f485ac7b7a51b41208703ef506ba72267643c12b
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-01-15 10:32:26 +0100

    fix(config): update API port configuration to use YOUR_API_PORT variable

--- your_api/src/main.ts
+++ your_api/src/main.ts
@@ -5,7 +5,7 @@ import { logger } from './modules/logger/providers/logger';
 export async function start(): Promise<void> {
   const app = await bootstrap();
   const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('port');
+  const PORT = configService.get<number>('YOUR_API_PORT');
 
   await app.listen(PORT ?? 3200, '0.0.0.0');
 }
